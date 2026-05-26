aca499a794ad1de6ff59e046f697b168547e8676
wolfitdm2 <significantbed@protonmail.com>
2026-05-26 04:17:36 +0200

    update

--- obj/Release/net462/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AliceInGradleDemosaicMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "AliceInGradleDemosaicMod";
-        public const string PLUGIN_NAME = "You have unlimited all!";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
